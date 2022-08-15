bc1791abd593bf030b36cf106540979a7694f1ef
krnl0138 <krnl03182@gmail.com>
2022-08-15 05:04:25 +0400

    add basic / markup
    Signed-off-by: krnl0138 <krnl03182@gmail.com>

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,34 +1,14 @@
-import styled from "styled-components";
 import { Layout } from "../components/Layout";
-import { SignUpForm } from "../features/loginForm/SignUpForm";
-
-const HeroSection = styled.section`
-  height: 80vh;
-  background-color: pink;
-`;
-
-const BenefitsSection = styled.section`
-  height: 80vh;
-  background-color: papayawhip;
-`;
-
-const SignUpSection = styled.section`
-  height: 80vh;
-  background-color: olive;
-`;
+import { BenefitsSection } from "../features/benefitsSection/BenefitsSection";
+import { HeroSection } from "../features/heroSection/HeroSection";
+import { SignUpSection } from "../features/signUpSection/SignUpSection";
 
 const Home = () => {
   return (
     <Layout>
-      <HeroSection>
-        <p>Hello</p>
-      </HeroSection>
-      <BenefitsSection>
-        <p>World</p>
-      </BenefitsSection>
-      <SignUpSection>
-        <SignUpForm />
-      </SignUpSection>
+      <HeroSection />
+      <BenefitsSection />
+      <SignUpSection />
     </Layout>
   );
 };
